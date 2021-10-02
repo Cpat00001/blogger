7f8c8c10ed2931bb60d06acba06b1f21931b8477
Pat <p171982@gmail.com>
2021-10-02 19:55:07 +0100

    changes

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -7,9 +7,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 use App\Entity\User;
-use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,36 +22,54 @@ class CommentController extends AbstractController
     /**
     * @Route("/comment", name="comment")
     */
-    public function commentForm(UserRepository $userRepository, Request $request): Response
+    public function commentForm(Request $request, UserInterface $user): Response
     {
-        $repository = $this->getDoctrine()->getRepository(User::class);
-        $users = $repository->findAll();
-
-
+        //$currentUser = $user;
+        //var_dump($currentUser);
+        $currentUsername = $user->getUserIdentifier();
+        //var_dump($currentUsername);      
         $comment = new Comment();
-
+        
+        //build a form
         $form = $this->createFormBuilder($comment)
-            ->add('comment', TextType::class, [
-                'attr' => ['style' => 'height: 200px', 'required' => true,]
+            ->add('comment', TextareaType::class, [
+                'attr' => ['style' => 'height: 200px; width: 80%',  'required' => true,]
             ])
             ->add('username', TextType::class,[
-                'data' => 'session user',
+                'data' => $currentUsername,
                 'disabled' => true,
             ])
             // ->add('added', DateType::class)
-            ->add('submit', SubmitType::class,[
-                'attr' => ['class' => 'Add Comment']
-            ])
-           ->getForm();
+            ->add('Add_Comment', SubmitType::class,)
+            ->getForm();
         //var_dump($form);
+        //processing form
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+            echo "<h1>Submitted</h1>";
+             // $form->getData() holds the submitted values
+            // but, the original `$comment` variable has also been updated
+            $comment = $form->getData();
+             //insert current date of created comment
+             $comment->setAdded(new \DateTime());
+             $comment->setUsername($currentUsername);
+             var_dump($comment);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            
+            //redirect to current page + show flash message
+            $this->addFlash('success', 'Your comment has been added');
+            //redirect
+            //return $this->redirectToRoute('welcome');
+        }
 
         $commentForm = "Comment Form";
         return $this->render('comment/commentForm.html.twig', [
             'commentForm' => $commentForm,
             'form' => $form->createView(),
-            'users' => $users,
-            //'username' => $username,
-            // 'com' => $comment,
         ]);
     }
 }
